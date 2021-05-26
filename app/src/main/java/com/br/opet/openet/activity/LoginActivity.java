ccada4d609647dc4cc12909dd79ec8f00ebbed74
Tiago Borica <tiago.borica@gmail.com>
2021-05-25 23:25:14 -0300

    login layout

--- app/src/main/java/com/br/opet/openet/activity/LoginActivity.java
+++ app/src/main/java/com/br/opet/openet/activity/LoginActivity.java
@@ -2,14 +2,20 @@
 package com.br.opet.openet.activity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.br.opet.openet.R;
 
-public class LoginActivity extends NoBarActivity {
+public class LoginActivity extends NoBarActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }

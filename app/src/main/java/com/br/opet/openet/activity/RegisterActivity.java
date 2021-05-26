da081285b8c5b7d3dc182f7c5a85684044fc717e
Bruno Moraes <moraes.7bruno@gmail.com>
2021-05-26 20:07:14 -0300

    teste

---
+++ app/src/main/java/com/br/opet/openet/activity/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.br.opet.openet.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.br.opet.openet.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}

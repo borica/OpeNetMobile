b73308f51e6b901ceff2b459db7933b25aafe367
Borica <tiago.borica@gmail.com>
2021-06-01 23:26:00 -0300

    Dashboard activity
    Friends fragment, added various new user interactions

---
+++ app/src/main/java/com/br/opet/openet/adapter/recyclerViewAdapter/FriendRequestRecyclerViewAdapter.java
@@ -0,0 +1,59 @@
+package com.br.opet.openet.adapter.recyclerViewAdapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.br.opet.openet.R;
+import com.br.opet.openet.model.FriendModel;
+
+import java.util.List;
+
+public class FriendRequestRecyclerViewAdapter extends RecyclerView.Adapter<FriendRequestRecyclerViewAdapter.MyViewHolder> {
+
+    Context mContext;
+    List<FriendModel> friendsArrayList;
+
+    public FriendRequestRecyclerViewAdapter(Context mContext, List<FriendModel> friends) {
+        this.mContext = mContext;
+        friendsArrayList = friends;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater li = LayoutInflater.from(mContext);
+        View v = li.inflate(R.layout.friend_request_recyclerview_row, parent, false);
+
+        return  new MyViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull FriendRequestRecyclerViewAdapter.MyViewHolder holder, int position) {
+        holder.friendNameTextView.setText(friendsArrayList.get(position).getName());
+        holder.courseNameTextView.setText(friendsArrayList.get(position).getCourse().getCourse());
+    }
+
+    @Override
+    public int getItemCount() {
+        return friendsArrayList.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        TextView friendNameTextView;
+        TextView courseNameTextView;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            friendNameTextView = itemView.findViewById(R.id.friendNameTextView);
+            courseNameTextView = itemView.findViewById(R.id.friendCourseTextView);
+        }
+    }
+
+}
--- app/src/main/java/com/br/opet/openet/fragment/dashboardFragments/FriendsFragment.java
+++ app/src/main/java/com/br/opet/openet/fragment/dashboardFragments/FriendsFragment.java
@@ -1,9 +1,12 @@
 package com.br.opet.openet.fragment.dashboardFragments;
 
+import android.content.res.ColorStateList;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -11,9 +14,14 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import com.br.opet.openet.R;
+import com.br.opet.openet.adapter.recyclerViewAdapter.FriendRequestRecyclerViewAdapter;
 import com.br.opet.openet.adapter.recyclerViewAdapter.FriendsRecyclerViewAdapter;
+import com.br.opet.openet.adapter.recyclerViewAdapter.FriendsSuggestRecyclerViewAdapter;
 import com.br.opet.openet.model.CourseModel;
 import com.br.opet.openet.model.FriendModel;
 
@@ -22,6 +30,14 @@ import java.util.List;
 
 public class FriendsFragment extends Fragment {
 
+    RelativeLayout friendsListRelativeLayout;
+    RelativeLayout friendRequestRelativeLayout;
+    RelativeLayout exploreFriendsRelativeLayout;
+
+    Button allFriendsButton;
+    Button friendRequestsButton;
+    Button exploreFriendsButton;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
@@ -29,9 +45,12 @@ public class FriendsFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_friends, container, false);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        instanciateObjects(getView());
 
         //Preparing mock friendsList
         List<FriendModel> friendsList = new ArrayList<FriendModel>();
@@ -45,5 +64,110 @@ public class FriendsFragment extends Fragment {
         RecyclerView friendsListRecyclerView = view.findViewById(R.id.friendsListRecyclerView);
         friendsListRecyclerView.setAdapter(new FriendsRecyclerViewAdapter(view.getContext(), friendsList));
         friendsListRecyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
+
+        //Preparing mock friendsList
+        List<FriendModel> friendsRecommendedList = new ArrayList<FriendModel>();
+        friendsRecommendedList.add(new FriendModel("Luiza Sonza", new CourseModel("Publicidade e Propaganda")));
+        friendsRecommendedList.add(new FriendModel("Linus Torvalds", new CourseModel("Ciência da Computação")));
+        friendsRecommendedList.add(new FriendModel("Átila Iamarino", new CourseModel("Biologia")));
+        friendsRecommendedList.add(new FriendModel("Jair Bolsonaro", new CourseModel("Administração")));
+        friendsRecommendedList.add(new FriendModel("Jeff Bezos", new CourseModel("Empreendedorismo")));
+
+        //FriendsFragment
+        RecyclerView friendsSuggestRecommendedListRecyclerView = view.findViewById(R.id.exploreRecommendedFriendsListRecyclerView);
+        friendsSuggestRecommendedListRecyclerView.setAdapter(new FriendsSuggestRecyclerViewAdapter(view.getContext(), friendsRecommendedList));
+        friendsSuggestRecommendedListRecyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
+
+        //Preparing mock friendsList
+        List<FriendModel> friendsAllList = new ArrayList<FriendModel>();
+        friendsAllList.add(new FriendModel("Luiza Sonza", new CourseModel("Publicidade e Propaganda")));
+        friendsAllList.add(new FriendModel("Linus Torvalds", new CourseModel("Ciência da Computação")));
+        friendsAllList.add(new FriendModel("Átila Iamarino", new CourseModel("Biologia")));
+        friendsAllList.add(new FriendModel("Jair Bolsonaro", new CourseModel("Administração")));
+        friendsAllList.add(new FriendModel("Jeff Bezos", new CourseModel("Empreendedorismo")));
+        friendsAllList.add(new FriendModel("Luiza Sonza", new CourseModel("Publicidade e Propaganda")));
+        friendsAllList.add(new FriendModel("Linus Torvalds", new CourseModel("Ciência da Computação")));
+        friendsAllList.add(new FriendModel("Átila Iamarino", new CourseModel("Biologia")));
+        friendsAllList.add(new FriendModel("Jair Bolsonaro", new CourseModel("Administração")));
+        friendsAllList.add(new FriendModel("Jeff Bezos", new CourseModel("Empreendedorismo")));
+
+        //FriendsFragment
+        RecyclerView friendsSuggestAllListRecyclerView = view.findViewById(R.id.exploreFriendsListRecyclerView);
+        friendsSuggestAllListRecyclerView.setAdapter(new FriendsSuggestRecyclerViewAdapter(view.getContext(), friendsAllList));
+        friendsSuggestAllListRecyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
+
+        //Preparing mock friendsList
+        List<FriendModel> friendRequestsList = new ArrayList<FriendModel>();
+        friendRequestsList.add(new FriendModel("Luiza Sonza", new CourseModel("Publicidade e Propaganda")));
+        friendRequestsList.add(new FriendModel("Linus Torvalds", new CourseModel("Ciência da Computação")));
+        friendRequestsList.add(new FriendModel("Átila Iamarino", new CourseModel("Biologia")));
+        friendRequestsList.add(new FriendModel("Jair Bolsonaro", new CourseModel("Administração")));
+        friendRequestsList.add(new FriendModel("Jeff Bezos", new CourseModel("Empreendedorismo")));
+        friendRequestsList.add(new FriendModel("Luiza Sonza", new CourseModel("Publicidade e Propaganda")));
+        friendRequestsList.add(new FriendModel("Linus Torvalds", new CourseModel("Ciência da Computação")));
+        friendRequestsList.add(new FriendModel("Átila Iamarino", new CourseModel("Biologia")));
+        friendRequestsList.add(new FriendModel("Jair Bolsonaro", new CourseModel("Administração")));
+        friendRequestsList.add(new FriendModel("Jeff Bezos", new CourseModel("Empreendedorismo")));
+
+        //FriendsFragment
+        RecyclerView friendRequestsListRecyclerView = view.findViewById(R.id.friendRequestListRecyclerView);
+        friendRequestsListRecyclerView.setAdapter(new FriendRequestRecyclerViewAdapter(view.getContext(), friendRequestsList));
+        friendRequestsListRecyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
     }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private void instanciateObjects(View v) {
+        this.friendsListRelativeLayout = getView().findViewById(R.id.friendsListRelativeLayout);
+        this.friendRequestRelativeLayout = getView().findViewById(R.id.friendRequestRelativeLayout);
+        this.exploreFriendsRelativeLayout = getView().findViewById(R.id.exploreFriendsRelativeLayout);
+
+        this.allFriendsButton = getView().findViewById(R.id.allFriendsButton);
+        this.friendRequestsButton = getView().findViewById(R.id.friendRequestsButton);
+        this.exploreFriendsButton = getView().findViewById(R.id.exploreFriendsButton);
+
+        allFriendsButton.setOnClickListener(v1 -> {
+            friendRequestRelativeLayout.setVisibility(View.GONE);
+            exploreFriendsRelativeLayout.setVisibility(View.GONE);
+            friendsListRelativeLayout.setVisibility(View.VISIBLE);
+
+            allFriendsButton.setBackgroundTintList(v.getContext().getResources().getColorStateList(R.color.openet_green));
+            allFriendsButton.setTextColor(getResources().getColor(R.color.white));
+
+            friendRequestsButton.setBackgroundTintList(v.getContext().getResources().getColorStateList(R.color.light_gray));
+            friendRequestsButton.setTextColor(getResources().getColor(R.color.unselect_button_text));
+            exploreFriendsButton.setBackgroundTintList(v.getContext().getResources().getColorStateList(R.color.light_gray));
+            exploreFriendsButton.setTextColor(getResources().getColor(R.color.unselect_button_text));
+
+        });
+
+        friendRequestsButton.setOnClickListener(v1 -> {
+            friendRequestRelativeLayout.setVisibility(View.VISIBLE);
+            exploreFriendsRelativeLayout.setVisibility(View.GONE);
+            friendsListRelativeLayout.setVisibility(View.GONE);
+
+            friendRequestsButton.setBackgroundTintList(v.getContext().getResources().getColorStateList(R.color.openet_green));
+            friendRequestsButton.setTextColor(getResources().getColor(R.color.white));
+
+            allFriendsButton.setBackgroundTintList(v.getContext().getResources().getColorStateList(R.color.light_gray));
+            allFriendsButton.setTextColor(getResources().getColor(R.color.unselect_button_text));
+            exploreFriendsButton.setBackgroundTintList(v.getContext().getResources().getColorStateList(R.color.light_gray));
+            exploreFriendsButton.setTextColor(getResources().getColor(R.color.unselect_button_text));
+        });
+
+        exploreFriendsButton.setOnClickListener(v1 -> {
+            friendRequestRelativeLayout.setVisibility(View.GONE);
+            exploreFriendsRelativeLayout.setVisibility(View.VISIBLE);
+            friendsListRelativeLayout.setVisibility(View.GONE);
+
+            exploreFriendsButton.setBackgroundTintList(v.getContext().getResources().getColorStateList(R.color.openet_green));
+            exploreFriendsButton.setTextColor(getResources().getColor(R.color.white));
+
+            allFriendsButton.setBackgroundTintList(v.getContext().getResources().getColorStateList(R.color.light_gray));
+            allFriendsButton.setTextColor(getResources().getColor(R.color.unselect_button_text));
+            friendRequestsButton.setBackgroundTintList(v.getContext().getResources().getColorStateList(R.color.light_gray));
+            friendRequestsButton.setTextColor(getResources().getColor(R.color.unselect_button_text));
+        });
+
+    }
+
 }
